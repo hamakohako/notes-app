0bed66ce3a178300d15ce97a708ef350afaceb86
digihakx <digihamako@gmail.com>
2022-02-18 15:31:04 +0800

    added initial models and migrate database

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ db_host = os.environ.get('DB_HOST', default='localhost')
 db_name = os.environ.get('DB_NAME', default='notesapp')
 db_user = os.environ.get('DB_USER', default='user')
 db_pass = os.environ.get('DB_PASS', default='')
-db_port = os.environ.get('DB_PORT', default='5432')
+db_port = os.environ.get('DB_PORT', default='80')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = f"postgres://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
